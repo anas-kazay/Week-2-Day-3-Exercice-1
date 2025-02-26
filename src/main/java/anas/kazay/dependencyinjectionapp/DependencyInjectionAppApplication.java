bdd076a1e5b9f055e0e621c239d51517d14523b8
anasKazay <anaskazay@gmail.com>
2025-02-26 14:41:58 +0000

    xml-injection

--- src/main/java/anas/kazay/dependencyinjectionapp/DependencyInjectionAppApplication.java
+++ src/main/java/anas/kazay/dependencyinjectionapp/DependencyInjectionAppApplication.java
@@ -1,13 +1,18 @@
 package anas.kazay.dependencyinjectionapp;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import anas.kazay.dependencyinjectionapp.decoupled.MessageRenderer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DependencyInjectionAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DependencyInjectionAppApplication.class, args);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext
+                ("spring/app-context.xml");
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
     }
 
 }
